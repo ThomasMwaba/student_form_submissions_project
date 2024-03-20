862b4fc6d220a96c5dc5f05329364477d331a6ab
ThomasMwaba <mwangelwa@hotmail.com>
2024-03-20 16:23:36 +0300

    changes made

--- db_setup.py
+++ db_setup.py
@@ -7,17 +7,17 @@ class Student(Base):
     # Define table name
     __tablename__ = "students"
     # Define columns
-    id = Column(Integer, primary_key=True, index=True)  # Primary key column
     name = Column(String, index=True)  # Column for student name
-    school_id = Column(Integer, index=True)  # Column for student school ID
+    id = Column(Integer, primary_key=True, index=True)  # Primary key column
     grade5 = Column(Boolean, default=False)  # Column for grade 5, default to False
     grade6 = Column(Boolean, default=False)  # Column for grade 6, default to False
     grade7 = Column(Boolean, default=False)  # Column for grade 7, default to False
-    project_name = Column(String, index=True)  # Column for project name
-    project_progress = Column(String, index=True)  # Column for project progress
-    project_hours = Column(Integer, index=True)  # Column for hours spent on the project
-    subject_name = Column(String, index=True)  # Column for subject name
-    study_method = Column(String, index=True)  # Column for study method
+    project= Column(String, index=True)  # Column for project name
+    progress = Column(String, index=True)  # Column for project progress
+    hours = Column(Integer, index=True)  # Column for hours spent on the project
+    subject = Column(String, index=True)  # Column for subject name
+    active = Column(String, index=True)  # Column for study method
+    method = Column(String, index = True)   # Column for how they did active recall
 
 # Create an instance of the table
 students_table = Student.__table__
--- main.py
+++ main.py
@@ -7,20 +7,20 @@ app = FastAPI()
 # Define a function to create a database connection pool
 async def get_pool():
     return await asyncpg.create_pool(
-        user='my-database',
+        user='postgres',
         password='Thomas4545',
-        database='student_database',
+        database='my-database',
         host='my-database.cniwyesmcch0.eu-west-2.rds.amazonaws.com',
         port=5432
     )
 
 @app.post("/submit_form")
-async def submit(first_name: str = Form(...),id: int = Form(...), grade5: bool = Form(False), grade6: bool = Form(False), grade7: bool = Form(False), project_name: str = Form(...), project_progress: str = Form(...), project_hours: float = Form(...), subject_name: str = Form(...), study_method: str = Form(...)):
+async def submit(name: str = Form(...),id: int = Form(...), grade5: bool = Form(False), grade6: bool = Form(False), grade7: bool = Form(False), project: str = Form(...), progress: str = Form(...), hours: int = Form(...), subject: str = Form(...), active: str = Form(...),method: str = Form(...)):
     # Access form fields like first name, grade, etc.
-    async with get_pool() as pool:
-        async with pool.acquire() as conn:
-            # Define the SQL query to insert form data into the database table
-            query = "INSERT INTO students (first_name,id, grade5, grade6, grade7, project_name, project_progress, project_hours, subject_name, study_method) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)"
-            # Execute the SQL query with form data as parameters
-            row = await conn.fetchrow(query, id, first_name, grade5, grade6, grade7, project_name, project_progress, project_hours, subject_name, study_method, subject_hours)
-            return row
+    pool = await get_pool()
+    async with pool.acquire() as conn:
+        # Define the SQL query to insert form data into the database table
+        query = "INSERT INTO students (name,id, grade5, grade6, grade7, project, progress, hours, subject, method) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10,$11)"
+        # Execute the SQL query with form data as parameters
+        row = await conn.fetchrow(query,name,id, grade5, grade6, grade7, project, progress, hours, subject, active, method)
+        return row
